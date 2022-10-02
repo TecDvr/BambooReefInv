d48433c6b57ac7b712aebd1f82caefce9fdc882c
TecDvr <zachgw@gmail.com>
2022-10-01 18:35:10 -0700

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import { Container } from "react-bootstrap";
+import Signup from "./Signup";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import ForgotPassword from "./ForgotPassword"
+import { AuthProvider } from "../context/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UpdateProfile from "./UpdateProfile";
+import PrivateRoute from "./PrivateRoute";
+
+const App = () => {
+  return (
+    
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={ {minHeight: "100vh"} }>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+        
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route exact path="/" element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }/>
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/update-profile" element={
+                  <PrivateRoute>
+                    <UpdateProfile />
+                  </PrivateRoute>
+                }/>
+              </Routes> 
+            </AuthProvider>
+          </Router>
+         
+        </div>
+        
+    </Container>
+    
+  )
+}
+
+export default App;
+ 
